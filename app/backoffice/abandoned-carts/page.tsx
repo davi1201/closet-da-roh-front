bc95b0cacea686c61aecbbc2894d9ff273923d56
Davi Alves <davi_ti@outlook.com>
2025-10-25 08:40:02 -0300

    ajuste de carrinhos abandonados

--- app/backoffice/abandoned-carts/page.tsx
+++ app/backoffice/abandoned-carts/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import ListAbandonedCarts from '@/domains/abandoned-carts/list-all-abandoned-carts';
+
 export default function AbandonedCartsPage() {
-  return <></>;
+  return <ListAbandonedCarts />;
 }
