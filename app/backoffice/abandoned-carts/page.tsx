3839699b8cb108446f5e19e7a0af5bb698504726
Davi Alves <davi_ti@outlook.com>
2025-10-25 08:28:43 -0300

    teste

--- app/backoffice/abandoned-carts/page.tsx
+++ app/backoffice/abandoned-carts/page.tsx
@@ -1,12 +1,5 @@
 'use client';
 
-import dynamic from 'next/dynamic';
-
-const ListAbandonedCarts = dynamic(
-  () => import('@/domains/abandoned-carts/list-all-abandoned-carts'),
-  { ssr: false }
-);
-
 export default function AbandonedCartsPage() {
-  return <ListAbandonedCarts />;
+  return <></>;
 }
